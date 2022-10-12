1c9bddae72e830045322e4107ca619f034dcc2c9
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-10-12 11:53:05 +0300

    add screens in the app file

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Provider } from 'react-redux';
-import HomeScreen from './screens/HomeScreen';
-import { StatusBar } from 'expo-status-bar';
-import RestaurantScreen from './screens/RestaurantScreen';
-import { store } from './store';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { StatusBar } from "expo-status-bar";
+import RestaurantScreen from "./screens/RestaurantScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
+import BasketScreen from "./screens/BasketScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
+import { store } from "./store";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +19,21 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{ presentation: "modal", headerShown: false }}
+          />
+          <Stack.Screen
+            name="PreparingOrder"
+            component={PreparingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
+          />
+          <Stack.Screen
+            name="Delivery"
+            component={DeliveryScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
+          />
         </Stack.Navigator>
         <StatusBar style="dark " />
       </Provider>
